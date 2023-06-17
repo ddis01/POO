de160fa23f0062ff4d8c75b75c3d3cbd269c6985
Darline <131263833+ddis01@users.noreply.github.com>
2023-06-17 10:37:13 -0300

    Aula de Sabado
    atividade do Alex e David

--- classes.js
+++ classes.js
@@ -1,19 +1,73 @@
-class Celular{
-    constructor(marca, modelo, ram, armazenamento, processador, qtdDeCameras, foto){
-        this.marca = marca
-        this.modelo = modelo
-        this.ram = ram
-        this.armazenamento = armazenamento
-        this.processador = processador
-        this.qtdDeCameras = qtdDeCameras + " Câmeras"
-        this.foto = foto
+class LojaDoBrasil {
+    constructor(nome, cnpj) {
+        this.nome = nome,
+            this.cnpj = cnpj;
     }
 }
 
-class Tv {
-    constructor(marca, modelo, tamanho){
-        this.marca = marca
-        this.modelo = modelo
-        this.tamanho = tamanho
+class Cliente {
+    constructor(nome, telefone, cpf, endereco,) {
+        this.nome = nome,
+            this.telefone = telefone,
+            this.cpf = cpf,
+            this.endereco = endereco;
     }
 }
+
+class Produto {
+    constructor(nome, precoUnitario) {
+        this.nome = nome,
+            this.precoUnitario = precoUnitario;
+
+    }
+}
+
+class Item {
+    constructor(produto, quantidade, precoTotal) {
+        this.quantidade = quantidade,
+            this.produto = produto,
+            this.precoTotal = precoTotal;
+    }
+    calcularPrecoTotal() {
+        return this.quantidade * this.precoUnitario
+    }
+}
+
+class Pedido {
+    constructor(loja, cliente, frete, numeroDoPedido, precoTotal, data, itensPedidos) {
+        this.numeroDoPedido = numeroDoPedido,
+            this.precoTotal = precoTotal,
+            this.data = data,
+            this.loja = loja,
+            this.cliente = cliente,
+            this.frete = frete,
+            this.itensPedidos = itensPedidos;
+    }
+
+    calcularSubtotal() {
+        let subtotal = 0;
+        for (let i = 0; i < this.itensPedidos.length; i++) {
+            this.subtotal += this.itensPedidos[i].calcularPrecoTotal()
+        }
+        return subtotal;
+    }
+
+    calcularTotal() {
+        return this.subtotal() + this.frete;
+    }
+
+    adicionarItem(item) {
+        this.itensPedidos.push(item)
+    }
+
+    removerItem(item) {
+        for (let i = 0; i < this.itensPedidos.length; i++) {
+            if (this.itensPedidos[i].item == item) {
+                this.itensPedidos.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+}
+
